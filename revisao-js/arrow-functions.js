072fda2a9d531c765f80555e5fceb51b09597b6b
AirleneA <airlene.antonelli@gmail.com>
2021-02-17 13:54:56 +0000

    (17/02) Continuação da revisão de JS

--- revisao-js/arrow-functions.js
+++ revisao-js/arrow-functions.js
@@ -31,3 +31,48 @@ console.log(potencia(2, 5))
 let potencia2 = (b, e) => b ** e
 
 console.log(potencia2(2, 5))
+
+// Função sem parâmetros
+function megasenna(){
+    //Esta função retorna um número aleatório entre 1 e 60
+    //Math.randow() -> retorna um numro aleatório entre 0(inclusive) e 1(inclusive)
+    // * 60 -> Ajusta a faixa de valores para entre 0 e 59
+    // + 1 -> Ajusta a faixa de valores para 1 a 60
+    // Math.floor() -> retira a parte fracionária (casas decimais)
+    return Math.floor(Mat.randow() * 60 + 1)
+
+// 6 números de um jogo da megasena
+console.log(megasena(), megasena(),megasena(), megasena(),megasena(), megasena())
+
+
+//Em arrow functions sem parâmetros, é necessário deixar um par de parênteses
+//para marcar o lugar dos parâmetros
+
+const megasena2 = () => Math.floor(Mat.randow() * 60 + 1)
+// 6 números de um jogo da megasena
+console.log(megasena2(), megasena2(),megasena2(), megasena2(),megasena2(), megasena2())
+}
+
+//Função com mais de uma linha de código no corpo
+function somaTudo(...nums){ // ... significa qua nao ha quantidade fixa de parâmetros
+    let soma = 0
+    // O parâmetro de resto, dentro de uma função, é interpretado como um vetor
+    for(let n of nums) soma += n
+    return soma 
+}
+
+console.log(somaTudo(1,2,3,4,5,6,7,8,9))
+
+// Em arrow functions com mais de uma linha no corpo, as chaves retornam e
+// também é necessário voltar a usar a palhavra return para retornar um valor
+
+const somaTudo2 = (...nums) => {
+let soma = 0
+    // O parâmetro de resto, dentro de uma função, é interpretado como um vetor
+    for(let n of nums) soma += n
+    return soma 
+}
+console.log(somaTudo2(1,2,3,4,5,6,7,8,9))
+
+//CONCLUSÃO: arrow functions são ideais para quando uma função tem apenas uma
+//ou poucas linhas de códigos no corpo.
